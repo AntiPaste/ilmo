f6e1903ab7d77b20dfea42f1525f621d7e2527a1
Kasper Koho <kasper.koho@taitounited.fi>
2016-03-05 16:57:46 +0200

    Added config parsing

---
+++ server/ilmo/config.py
@@ -0,0 +1,16 @@
+import os
+import json
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', help='specify config file location')
+args = parser.parse_args()
+filepath = args.config
+
+if not os.path.exists(filepath) or not os.path.isfile(filepath):
+    raise EnvironmentError('No valid file specified: {}'.format(filepath))
+
+with open(filepath, 'r') as config_file:
+    config = json.load(config_file)
+    print('Loaded environment "{}"'.format(config['environment']))
